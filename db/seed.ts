381186a2185295e56ec8d607ee8e51ecbbeaf81e
farookquisar <farook.quisar@gmail.com>
2024-05-18 12:56:04 +0300

    Update seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,15 @@
-import { db } from 'astro:db';
+import { Contact, db } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO 
+	await db.insert(Contact).values([
+		{
+			first_name: "Farook",
+			last_name: "Quisar",
+			email_address: "5CfZ7@example.com",
+			query_type: "general-enquiry",
+			message: "Hi there!",
+			consent: true,
+		},
+	]);
 }
